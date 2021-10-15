cf10d1387469796b37a7887e06f3b1018b485e1f
Barbara Pereira <barbara.pereira1990@gmail.com>
2021-10-15 10:27:53 -0300

    exemplos funcaoes arrow

--- fundamentos/funcao2.js
+++ fundamentos/funcao2.js
@@ -2,5 +2,14 @@
 const imprimirSoma = function (a, b) {
   console.log(a + b);
 }
-
 imprimirSoma(2, 3);
+
+//Armazenando uma função arrow numa variavel / constante
+const soma = (a, b) => {
+  return a, b;
+}
+console.log(soma(1, 3));
+
+//arrow function com retorno implícito 
+const subtracao = (a, b) => a - b;
+console.log(2, 3);
