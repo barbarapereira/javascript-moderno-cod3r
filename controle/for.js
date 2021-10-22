df31f52257d51a06ecf923179189cebb2f80abfd
Barbara Pereira <barbara.pereira1990@gmail.com>
2021-10-22 14:12:51 -0300

    controle repetição - for

---
+++ controle/for.js
@@ -0,0 +1,14 @@
+let contador = 1;
+while(contador <= 10) {
+  console.log(`contador: ${contador}`);
+  contador++
+}
+
+for(let i= 1; i<= 10; i++) {
+  console.log(`i:${i}`);
+}
+
+const notas = [7.1, 8.9, 3.1, 0, 7.1];
+for(i = 0; i < notas.length; i++) {
+  console.log(`nota ${i}: ${notas[i]}`)
+}
